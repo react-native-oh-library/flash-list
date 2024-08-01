4507779eea8210fa143924daed5708c039ae956d
Louis Chan <61399097+Louis-C7@users.noreply.github.com>
2024-08-01 16:35:53 +0800

    chore: add example App (tester) (#20)

---
+++ tester/FlashListDemo/App.tsx
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {Platform, UIManager} from 'react-native';
+
+import {DebugContextProvider} from './Debug';
+import NavigationTree from './NavigationTree';
+
+const App = () => {
+  if (Platform.OS === 'android') {
+    if (UIManager.setLayoutAnimationEnabledExperimental) {
+      UIManager.setLayoutAnimationEnabledExperimental(true);
+    }
+  }
+
+  return (
+    <DebugContextProvider>
+      <NavigationTree />
+    </DebugContextProvider>
+  );
+};
+
+export default App;
